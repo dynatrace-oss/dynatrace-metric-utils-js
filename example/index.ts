eae06c8312b82c432a92116e59ca2ef9e4119941
Daniel Dyla <dyladan@users.noreply.github.com>
2021-06-18 11:05:03 -0400

    Add example

--- example/index.ts
+++ example/index.ts
@@ -1,23 +1,59 @@
 /*
-    Copyright 2020 Dynatrace LLC
+Copyright 2020 Dynatrace LLC
 
-    Licensed under the Apache License, Version 2.0 (the "License");
-    you may not use this file except in compliance with the License.
-    You may obtain a copy of the License at
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
 
-    http://www.apache.org/licenses/LICENSE-2.0
+http://www.apache.org/licenses/LICENSE-2.0
 
-    Unless required by applicable law or agreed to in writing, software
-    distributed under the License is distributed on an "AS IS" BASIS,
-    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-    See the License for the specific language governing permissions and
-    limitations under the License.
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
 */
 
-import { MetricFactory } from "..";
+/* eslint-disable no-console */
+
+import { getDefaultOneAgentEndpoint, getOneAgentMetadata, getPayloadLinesLimit, MetricFactory } from "../src";
 
 const factory = new MetricFactory({
-    defaultDimensions: [],
-    oneAgentMetadata: [],
+    defaultDimensions: [{ key: "this_is_a_key", value: "and its value" }],
+    oneAgentMetadata: getOneAgentMetadata(),
     prefix: "prefix"
 });
+
+const gauge = factory.createGauge("my_gauge", [{ key: "metric_dimension_key", value: "and value" }], 23, new Date());
+const deltaCounter = factory.createDeltaCounter("my_delta_counter", [{ key: "metric_dimension_key", value: "and value" }], 23, new Date());
+const totalCounter = factory.createTotalCounter("my_cumulative_counter", [{ key: "metric_dimension_key", value: "and value" }], 23, new Date());
+const summary = factory.createSummary("my_delta_counter", [{ key: "metric_dimension_key", value: "and value" }], { min: 3, max: 32, count: 12, sum: 53 }, new Date());
+
+console.log("Dynatrace API Constants:");
+console.log(`Limit lines per payload:     ${getPayloadLinesLimit()}`);
+console.log(`Default OneAgent endpoint:   ${getDefaultOneAgentEndpoint()}`);
+console.log("");
+
+if (gauge == null) {
+    console.error("gauge normalization failed");
+} else {
+    console.log(gauge.serialize());
+}
+
+if (deltaCounter == null) {
+    console.error("deltaCounter normalization failed");
+} else {
+    console.log(deltaCounter.serialize());
+}
+
+if (totalCounter == null) {
+    console.error("totalCounter normalization failed");
+} else {
+    console.log(totalCounter.serialize());
+}
+
+if (summary == null) {
+    console.error("summary normalization failed");
+} else {
+    console.log(summary.serialize());
+}
